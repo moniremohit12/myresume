c303045bdd5e79d68cc1bff529118d19d4c0f7fc
Monire Mohit <mohit753357@gmail.com>
2020-07-03 18:21:40 +0430

    Updates

---
+++ precache-manifest.976fc833134d5d4486276f67d1eecaf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934c62b2b14e70613852f38512f59025",
+    "url": "/myresume/index.html"
+  },
+  {
+    "revision": "9a3c5fbeb4d25ce54af8",
+    "url": "/myresume/static/css/main.51dd67f1.chunk.css"
+  },
+  {
+    "revision": "9911e5686d70265086ad",
+    "url": "/myresume/static/js/2.264827a2.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/myresume/static/js/2.264827a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a3c5fbeb4d25ce54af8",
+    "url": "/myresume/static/js/main.a4f2a59a.chunk.js"
+  },
+  {
+    "revision": "ad018f2c61089d1aa48f",
+    "url": "/myresume/static/js/runtime-main.f1cbb89b.js"
+  }
+]);
